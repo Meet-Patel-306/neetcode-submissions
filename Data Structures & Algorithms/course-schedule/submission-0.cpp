a62ca48cfd13891ab18067c77b1550a9aa7a1256
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2026-07-28 23:37:06 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(auto i : prerequisites){
+            adj[i[1]].push_back(i[0]);
+        }
+        vector<int> ind(numCourses,0);
+        for(auto i:adj){
+            for(int j:i){
+                ind[j]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0;i<numCourses;i++){
+            if(ind[i] == 0) q.push(i);
+        }
+        int ans = 0;
+        while(!q.empty()){
+            int top = q.front();
+            q.pop();
+            ans++;
+            for(int i:adj[top]){
+                ind[i]--;
+                if(ind[i] == 0) q.push(i);
+            }
+        }
+        return ans!=numCourses ? false:true;
+    }
+};
